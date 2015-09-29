0d36bbe857bb55ec42b1dc6e22849df3a1fc7ac1
Chris Parker <mrcsparker@gmail.com>
2015-09-28 23:56:09 -0500

    File parsing added

--- paxpat/Expat.hpp
+++ paxpat/Expat.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <iostream>
 #include <map>
 #include <expat.h>
@@ -7,57 +9,87 @@ using namespace std;
 namespace paxpat {
     class Expat {
     public:
-
+        
         Expat() {
             parser = XML_ParserCreate(NULL);
             if (!parser) {
                 throw std::bad_alloc();
             }
-
+            
             // pass ref from Expat(class) to the expat parser
             XML_SetUserData(parser, this);
-
+            
             // callbacks for the elements handlers
             XML_SetElementHandler(parser, &Expat::startElement, &Expat::endElement);
         }
-
+        
         ~Expat() {
             XML_ParserFree(parser);
         }
-
+        
         long line() {
             return XML_GetCurrentLineNumber(parser);
         }
-
+        
         long column() {
             return XML_GetCurrentColumnNumber(parser);
         }
-
+        
         void parseString(string s) {
             int len = static_cast<int>(strlen(s.c_str()));
             XML_Parse(parser, s.c_str(), len, 1);
         }
-
+        
+        void parseFile(const std::string& filename) {
+            const size_t BUFFER_SIZE = 255;
+            
+            FILE* fd = fopen(filename.c_str(), "r");
+            if (fd == NULL) {
+                throw std::runtime_error("File does not exist");
+            }
+            
+            for (;;) {
+                void *buffer = XML_GetBuffer(parser, BUFFER_SIZE);
+                if (buffer == NULL) {
+                    throw std::runtime_error("out of memory");
+                }
+                
+                int bytes_read = static_cast<int>(fread(buffer, 1, BUFFER_SIZE, fd));
+                if (bytes_read < 0) {
+                    throw std::runtime_error("error reading file");
+                }
+                
+                if (!XML_ParseBuffer(parser, bytes_read, bytes_read == 0)) {
+                    throw std::runtime_error("could not parse buffer");
+                }
+                
+                if (bytes_read == 0) {
+                    break;
+                }
+            }
+            fclose(fd);
+        }
+        
         void elementHandler(string elementName, function<void(Expat& a)> call) {
             elementCallbacks[elementName] = call;
         }
-
+        
     private:
-
+        
         XML_Parser parser;
-
+        
         static void startElement(void *data, const char *name, const char **atts) {
-
+            
             if (static_cast<Expat *>(data)->elementCallbacks.find(name) !=
-                    static_cast<Expat *>(data)->elementCallbacks.end()) {
+                static_cast<Expat *>(data)->elementCallbacks.end()) {
                 static_cast<Expat *>(data)->elementCallbacks.find(name)->second(*static_cast<Expat *>(data));
             }
         }
-
+        
         static void endElement(void *data, const char *name) {
-
+            
         }
-
+        
         map<string, function<void(Expat& a)>> elementCallbacks;
     };
 }
--- paxpat/main.cpp
+++ paxpat/main.cpp
@@ -1,22 +1,24 @@
 #include <iostream>
-#include "src/Expat.hpp"
+#include <fstream>
+#include "Expat.hpp"
 
 using namespace std;
 using namespace paxpat;
 
 int main(int argc, const char * argv[]) {
-
+    
     string xml = "<?xml version=\"1.0\"?><catalog><book id=\"bk101\"><author>Gambardella, Matthew</author><title>XML Developer's Guide</title></book></catalog>";
-
+    
     Expat e;
-
+    
     e.elementHandler("catalog", [](Expat& a) -> void {
         cout << "found catalog on line " << a.line() << endl;
     });
     e.elementHandler("author", [](Expat& a) -> void {
         cout << "found author on column " << a.column() << endl;
     });
-    e.parseString(xml);
-
+    e.parseFile("books.xml");
+    
+    
     return 0;
 }
